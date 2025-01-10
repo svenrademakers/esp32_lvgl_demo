8af14b5e513524f335ac04f00d37a18b5d63815c
Sven Rademakers <sven.rademakers@gmail.com>
2025-01-10 13:03:37 +0000

    enable display backlight

--- docker_build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm --device=/dev/ttyACM0 --privileged --user "$(id -u):$(id -g)" -v $(pwd):/project -w /project -e HOME=/tmp espressif/idf idf.py build
--- main/main.c
+++ main/main.c
@@ -1,9 +1,53 @@
+#include "driver/ledc.h"
+#include "driver/spi_master.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "hal/ledc_types.h"
 #include "lvgl.h"
 #include <stdio.h>
 
-void app_main(void) {
+#define DISP_TAG "DISP_INIT"
 
-  printf("GOING TO INIT BITCHES\n");
+void init_backlight(void) {
+  esp_err_t ret;
+  ledc_timer_config_t ledc_timer = {
+      .speed_mode = LEDC_LOW_SPEED_MODE,   // Low-speed mode
+      .timer_num = 0,                      // Timer 0
+      .duty_resolution = LEDC_TIMER_5_BIT, // 5-bit resolution
+      .freq_hz = 24000,                    // Frequency 24 kHz
+      .clk_cfg = LEDC_APB_CLK              // Use APB clock source
+  };
+
+  ret = ledc_timer_config(&ledc_timer);
+  if (ret != ESP_OK) {
+    ESP_LOGE(DISP_TAG, "Failed to configure LEDC timer: %s",
+             esp_err_to_name(ret));
+    return;
+  }
+  ESP_LOGI(DISP_TAG, "LEDC timer configured");
+
+  // Set up LEDC channel configuration
+  ledc_channel_config_t ledc_channel = {
+      .gpio_num = 15,
+      .speed_mode = LEDC_LOW_SPEED_MODE,
+      .channel = LEDC_CHANNEL_0,
+      .timer_sel = LEDC_TIMER_0,
+      .duty = 32,
+  };
+
+  ret = ledc_channel_config(&ledc_channel);
+  if (ret != ESP_OK) {
+    ESP_LOGE(DISP_TAG, "Failed to configure LEDC channel: %s",
+             esp_err_to_name(ret));
+    return;
+  }
+  ESP_LOGI(DISP_TAG, "LEDC channel configured");
+}
+
+void app_main(void) {
+  ESP_LOGI("SVEN", "TEST");
+  // lv_obj_t *lottie = lv_lottie_create(lv_screen_active());
   lv_init();
+  init_backlight();
   printf("all done\n");
 }
